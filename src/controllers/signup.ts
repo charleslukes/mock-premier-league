dd9d074e87bcec86e91f19e43fc919bbe4a77afd
Charles Chiakwa <charleslukes28@gmail.com>
2019-09-01 16:18:32 +0100

    update the sign up control include token

--- src/controllers/signup.ts
+++ src/controllers/signup.ts
@@ -8,17 +8,20 @@ export const signup = async (req: Request, res: Response) => {
   if (error) return res.status(404).send({ error: error.details[0].message });
 
   try {
-    // finc if the user already exits
-    console.log(req.body);
+    // find if the user already exits
     const { name, email } = req.body;
 
-    const user = await new User(req.body);
+    let user = await User.findOne({ email });
+    if (user) return res.status(400).send(`Email already in use`);
+
+    user = await new User(req.body);
     const salt = await bcrypt.genSalt(10);
     user.password = await bcrypt.hash(user.password, salt);
     await user.save();
 
     const data = { name, email };
-    return res.status(200).send({
+    const token = user.getAuthToken();
+    res.header("x-auth-token", token).send({
       output: "sign up successfully",
       data
     });
